03d04aba476cdb289f565d975b7af7897f097902
Tenzin wangyal <tenzinwangyal20@yahoo.com>
2023-02-07 17:37:13 -0500

    Initial Commit

---
+++ 20200207-TenzinWangyal-NYCSchools/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  20200207-TenzinWangyal-NYCSchools
+//
+//  Created by Tenzin wangyal on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 20200207-TenzinWangyal-NYCSchools/_0200207_TenzinWangyal_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0200207_TenzinWangyal_NYCSchoolsApp.swift
+//  20200207-TenzinWangyal-NYCSchools
+//
+//  Created by Tenzin wangyal on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct _0200207_TenzinWangyal_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
